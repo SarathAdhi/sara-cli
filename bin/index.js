1fee29f8a6e5cf25a98ec256fe96fdd64cceef8f
SarathAdhi <s.sarathadhithya@gmail.com>
2022-05-31 12:00:00 +0530

    code bug fixed

--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,34 @@
 #!/usr/bin/env node
-
-import figlet from "figlet";
-import inquirer from "inquirer";
-import gradient from "gradient-string";
-import ora from "ora";
-import shell from "shelljs";
-import { appendFile } from "fs";
-import yargs from "yargs";
-import chalk from "chalk";
-shell.config.silent = true;
-
-const argv = yargs(process.argv.slice(2));
-
-function success(message) {
-  console.log(chalk.bold.greenBright(message));
+import d from "figlet";
+import e from "inquirer";
+import f from "gradient-string";
+import g from "ora";
+import a from "shelljs";
+import { appendFile as h } from "fs";
+import b from "yargs";
+import i from "chalk";
+a.config.silent = !0;
+let j = b(process.argv.slice(2));
+function k(a) {
+  console.log(i.bold.greenBright(a));
 }
-function warning(message) {
-  console.log(chalk.yellow(message));
+function l(a) {
+  console.log(i.yellow(a));
 }
-function error(message) {
-  console.log(chalk.bold.redBright(message));
+function m(a) {
+  console.log(i.bold.redBright(a));
 }
-function bgWhite(message) {
-  console.log(chalk.inverse.bold(message));
+function n(a) {
+  console.log(i.inverse.bold(a));
 }
-
-async function promptOptions(_prompt) {
-  const result = await inquirer.prompt(_prompt).catch((error) => {
-    if (error.isTtyError) {
-      error(error);
-      shell.exit(1);
-    } else {
-      error(error);
-      shell.exit(1);
-    }
+async function o(b) {
+  let c = await e.prompt(b).catch((b) => {
+    b.isTtyError, b(b), a.exit(1);
   });
-  return result;
+  return c;
 }
-
-async function openVsCode() {
-  const { response } = await promptOptions([
+async function p() {
+  let { response: b } = await o([
     {
       name: "response",
       message: "Would you like to the project in VS Code?",
@@ -47,85 +36,65 @@ async function openVsCode() {
       choices: ["yes", "no"],
     },
   ]);
-  if (response === "yes") {
-    shell.exec("code .", (code) => {
-      if (code !== 0) {
-        warning(
-          "\nProject created successfully. Failed to open VS Code or VS Code not found"
-        );
-        shell.exit(1);
-      } else {
-        success("\nSuccessfully opened the project in VS code.");
-        shell.exit(1);
-      }
+  "yes" === b &&
+    a.exec("code .", (b) => {
+      0 !== b
+        ? (l(
+            "\nProject created successfully. Failed to open VS Code or VS Code not found"
+          ),
+          a.exit(1))
+        : (k("\nSuccessfully opened the project in VS code."), a.exit(1));
     });
-  }
 }
-
-function installTailwind(fileName) {
-  const spinner = ora("Installing Tailwind CSS...").start();
-  shell.cd(fileName);
-
-  shell.exec(
-    `npm install -D tailwindcss postcss autoprefixer`,
-    async (code) => {
-      if (code !== 0) {
-        error("Error: While installing create react app");
-        shell.exit(1);
-      } else {
-        spinner.succeed();
-        shell.exec("npx tailwindcss init -p");
-        success("\nSuccessfully installed Tailwind CSS.");
-        shell.rm("tailwind.config.js");
-        appendFile(
-          "tailwind.config.js",
-          `module.exports = {
-          content: ["./src/**/*.{js,jsx,ts,tsx}"],
-          theme: {
-            extend: {},
-          },
-          plugins: [],
-        };
-        `,
-          (res, err) => {
-            if (err) {
-              error(err);
-            } else {
-              success(
-                "Successfully configured " +
-                  gradient.cristal("'Tailwind.config.js'\n")
-              );
+function q(b) {
+  let c = g("Installing Tailwind CSS...").start();
+  a.cd(b),
+    a.exec("npm install -D tailwindcss postcss autoprefixer", async (b) => {
+      0 !== b
+        ? (m("Error: While installing create react app"), a.exit(1))
+        : (c.succeed(),
+          a.exec("npx tailwindcss init -p"),
+          k("\nSuccessfully installed Tailwind CSS."),
+          a.rm("tailwind.config.js"),
+          h(
+            "tailwind.config.js",
+            `module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+`,
+            (b, a) => {
+              a
+                ? m(a)
+                : k(
+                    "Successfully configured " +
+                      f.cristal("'Tailwind.config.js'\n")
+                  );
             }
-          }
-        );
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        await openVsCode();
-      }
-    }
-  );
+          ),
+          await new Promise((a) => setTimeout(a, 500)),
+          await p());
+    });
 }
-
-async function installFramework(framework) {
-  const { fileName } = await promptOptions([
-    {
-      type: "input",
-      name: "fileName",
-      message: "Enter the project name?",
-    },
-  ]);
-
-  const spinner = ora(`Running create-${framework}-app...`).start();
-  shell.exec(`npx create-${framework}-app ${fileName}`, async (code) => {
-    if (code !== 0) {
-      error(
-        `\nError While installing create-${framework}-app. Try changing the file name.`
-      );
-      shell.exit(1);
-    } else {
-      spinner.succeed();
-      success(`\nInstalled create-${framework}-app successfully.\n`);
-
-      const { dependencies } = await promptOptions([
+async function r(b) {
+  let { fileName: c } = await o([
+      { type: "input", name: "fileName", message: "Enter the project name?" },
+    ]),
+    d = g(`Running create-${b}-app...`).start();
+  a.exec(`npx create-${b}-app ${c}`, async (e) => {
+    if (0 !== e)
+      m(`
+Error While installing create-${b}-app. Try changing the file name.`),
+        a.exit(1);
+    else {
+      d.succeed(),
+        k(`
+Installed create-${b}-app successfully.
+`);
+      let { dependencies: f } = await o([
         {
           name: "dependencies",
           message: "Would you like to install the following dependencies?",
@@ -133,54 +102,36 @@ async function installFramework(framework) {
           choices: ["Plain project", "Tailwind CSS"],
         },
       ]);
-      if (dependencies === "Tailwind CSS") installTailwind(fileName);
-      else {
-        success("Done.\n");
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        shell.cd(fileName);
-        await openVsCode();
-      }
+      "Tailwind CSS" === f
+        ? q(c)
+        : (k("Done.\n"),
+          await new Promise((a) => setTimeout(a, 500)),
+          a.cd(c),
+          await p());
     }
   });
 }
-
-async function main() {
-  // Displaying Geeks CLI
-  figlet("Sara CLI", function (err, data) {
-    console.log(gradient.pastel.multiline(data));
-  });
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  const { framework } = await promptOptions([
+async function s() {
+  d("Sara CLI", function (b, a) {
+    console.log(f.pastel.multiline(a));
+  }),
+    await new Promise((a) => setTimeout(a, 1e3));
+  let { framework: a } = await o([
     {
       name: "framework",
-      message: "Which framework would you like to",
+      message: "Which framework would you like to use?",
       type: "list",
       choices: ["React js", "Next js"],
     },
   ]);
-  if (framework === "React js") installFramework("react");
-  else installFramework("next");
+  "React js" === a ? r("react") : r("next");
 }
-
-// main();
-const init = () => {
-  argv
-    .command("init", "Create a project by selecting a language.", {
-      js: {
-        describe: "Language - JavaScript",
-      },
-      ts: {
-        describe: "Language - TypeScript",
-      },
-    })
-    .example("sara init --js or sara init --ts");
-
-  const arg = argv.argv;
-  if (arg.js && arg._[0] === "init") main();
-  else argv.showHelp();
+let c = () => {
+  j.command("init", "Create a project by selecting a language.", {
+    js: { describe: "Language - JavaScript" },
+    ts: { describe: "Language - TypeScript" },
+  }).example("sara init --js or sara init --ts");
+  let a = j.argv;
+  a.js && "init" === a._[0] ? s() : j.showHelp();
 };
-init();
-
-// const greeting = `Hello, ${options.name}!`;
-// test();
+c();
